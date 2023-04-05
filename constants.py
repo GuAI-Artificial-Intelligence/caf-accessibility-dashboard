926756a27bfe584518866e99bc437e8c7b455197
Cristian Torres <cftmontenegro@gmail.com>
2023-04-04 21:52:23 -0500

    first version with accesibility index

--- constants.py
+++ constants.py
@@ -22,8 +22,10 @@ SCATTER_ID = "my-scatter"
 MAP_ID = 'my-map'
 CITY_SELECTOR = 'my-city-selector'
 ACCESIBILITY_SELECTOR='my-accesibility-selector'
+CATEGORY_SELECTOR = 'my-category-selector'
+VARIABLE_SELECTOR = 'my-variable-selector'
 
-CATEGORICAL_VARIABLES = ['NSE_3', 'NSE_5']
+CATEGORICAL_VARIABLES = ['IndiAcce', 'NSE_5']
 NON_CATEGORICAL_VARIABLES = ['Poblacion']
 
 NSE_3_DICTMAP = {
@@ -40,19 +42,31 @@ NSE_5_DICTMAP = {
     '5 - Bajo': 1         
 }
 
-NS3_COLORSCALE = ['#311a3c', '#b63c3f', '#daa98a']
+INDIACCE_DICTMAP = {
+    '1. Alta': 4,
+    '2. Media Alta': 3,
+    '3. Media Baja': 2,
+    '4. Baja': 1
+}
+
+#NS3_COLORSCALE = ['#311a3c', '#b63c3f', '#daa98a']
 NS3_TICKVALS = [1, 2, 3]
 NS3_TICKTEXT=['Bajo',  'Medio', 'Alto']
 
-NS5_COLORSCALE = ['#311a3c', '#922651', '#b63c3f', '#ae6045', '#daa98a']
+#NS5_COLORSCALE = ['#311a3c', '#922651', '#b63c3f', '#ae6045', '#daa98a']
 NS5_TICKVALS = [1, 2, 3, 4, 5]
 NS5_TICKTEXT=['Bajo', 'Medio-Bajo', 'Medio','Medio-Alto', 'Alto']
+
+INDIACCE_TICKVALS = [1, 2, 3, 4]
+INDIACCE_TICKTEXT=['Baja', 'Media Baja', 'Media Alta','Alta']
+INDIACCE_COLORSCALE = ['#cabda7', '#b39d7d', '#9d7f54', '#69462a']
+
 
 CATEGORICAL_COLORBAR=dict(
     orientation='v',
     thickness=10,
     y=0.2,
-    x=0.88,
-    len=0.25,
+    x=0.86,
+    len=0.3,
     tickfont=dict(color="#323232"),
 )
--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 # Dash
+import dash
 from dash import Dash, dcc, html, Input, Output, ctx
 
 # Bootstrap
@@ -35,25 +36,25 @@ bogota_cuenca_gdf_geo = gpd.read_file(
     current_path / 'data' / 'bogota_cuenca_geo_v1.geojson',
 )
 
-
 geojson_bogota_cuenca = bogota_cuenca_gdf_geo.geometry
 
 
 
-def init_map(df=bogota_cuenca_df_csv, geodf=geojson_bogota_cuenca, variable='NSE_5', city=constants.BOGOTA_STR):
-    z = df[variable].map(constants.NSE_5_DICTMAP).values
+def init_map(df=bogota_cuenca_df_csv, geodf=geojson_bogota_cuenca, variable='IndiAcce', city=constants.BOGOTA_STR):
+    z = df[variable].map(constants.INDIACCE_DICTMAP).values
+    # z = df[variable]
     colorbar = constants.CATEGORICAL_COLORBAR
-    colorbar['tickvals'] = constants.NS5_TICKVALS
-    colorbar['ticktext'] = constants.NS5_TICKTEXT
-    colorbar['title'] = '<b>Nivel socio<br>económico</b><br> .'
+    colorbar['tickvals'] = constants.INDIACCE_TICKVALS
+    colorbar['ticktext'] = constants.INDIACCE_TICKTEXT
+    colorbar['title'] = '<b>Accesibilidad</b><br> .'
     fig_hex_map = go.Figure(
         go.Choroplethmapbox(
             geojson=json.loads(geodf.to_json()), 
             z=z,
             locations=pd.Series(df.index.values).astype(str), 
             colorbar=colorbar,
-            colorscale=constants.NS5_COLORSCALE,
-            marker_opacity=0.4
+            colorscale='Magma',
+            marker_opacity=1
         )
     )
 
@@ -63,7 +64,7 @@ def init_map(df=bogota_cuenca_df_csv, geodf=geojson_bogota_cuenca, variable='NSE
             "lat": constants.CENTER_CITY_COORDINATES[city]['center_lat'], 
             "lon": constants.CENTER_CITY_COORDINATES[city]['center_lon']
         },
-        mapbox_zoom=10,
+        mapbox_zoom=9.5,
         margin={"r": 0, "t": 0, "l": 0, "b": 0}
     )
     return fig_hex_map
@@ -81,7 +82,7 @@ fig_hex_map = init_map()
 
 
 
-def update_hex_map(city, fig_hex_map, variable=constants.CATEGORICAL_VARIABLES[1], df=bogota_cuenca_df_csv):
+def update_hex_map(city, fig_hex_map, variable='IndiAcce', df=bogota_cuenca_df_csv):
 
 
     lat = constants.CENTER_CITY_COORDINATES[city]['center_lat']
@@ -92,7 +93,7 @@ def update_hex_map(city, fig_hex_map, variable=constants.CATEGORICAL_VARIABLES[1
         nse_variable = True
 
     if city==constants.BOGOTA_STR:
-        zoom = 10
+        zoom = 9.5
 
     if city==constants.CUENCA_STR:
         zoom = 11.5
@@ -100,16 +101,17 @@ def update_hex_map(city, fig_hex_map, variable=constants.CATEGORICAL_VARIABLES[1
     if nse_variable:
         colorbar = constants.CATEGORICAL_COLORBAR
         if variable==constants.CATEGORICAL_VARIABLES[0]:
-            z = df[variable].map(constants.NSE_3_DICTMAP).values
-            colorbar['tickvals'] = constants.NS3_TICKVALS
-            colorbar['ticktext'] = constants.NS3_TICKTEXT
-            zmax=3
+            z = df[variable].map(constants.INDIACCE_DICTMAP).values
+            colorbar['tickvals'] = constants.INDIACCE_TICKVALS
+            colorbar['ticktext'] = constants.INDIACCE_TICKTEXT
+            zmax=4
+            colorbar['title'] = '<b>Accesibilidad</b><br> .'
         if variable==constants.CATEGORICAL_VARIABLES[1]:
             z = df[variable].map(constants.NSE_5_DICTMAP).values
             colorbar['tickvals'] = constants.NS5_TICKVALS
             colorbar['ticktext'] = constants.NS5_TICKTEXT
             zmax=5
-        colorbar['title'] = '<b>Nivel socio<br>económico</b><br> .'
+            colorbar['title'] = '<b>Nivel socio<br>económico</b><br> .'
         # fig_hex_map.update_traces(
         #     overwrite=True,
         #     z=z,
@@ -149,6 +151,67 @@ def update_hex_map(city, fig_hex_map, variable=constants.CATEGORICAL_VARIABLES[1
     return fig_hex_map
 
 
+def get_bar_figure():
+
+    nse = bogota_cuenca_df_csv.NSE_5.unique()
+    nse = [    
+        '1 - Alto',          
+        '2 - Medio-Alto',     
+        '3 - Medio',   
+        '4 - Medio-Bajo',     
+        '5 - Bajo' 
+    ]
+    y1 = bogota_cuenca_df_csv[bogota_cuenca_df_csv.IndiAcce=='1. Alta'][['NSE_5', 'Poblacion']].groupby('NSE_5').sum()['Poblacion'].values
+    y2 = bogota_cuenca_df_csv[bogota_cuenca_df_csv.IndiAcce=='2. Media Alta'][['NSE_5', 'Poblacion']].groupby('NSE_5').sum()['Poblacion'].values
+    y3 = bogota_cuenca_df_csv[bogota_cuenca_df_csv.IndiAcce=='3. Media Baja'][['NSE_5', 'Poblacion']].groupby('NSE_5').sum()['Poblacion'].values
+    y4 = bogota_cuenca_df_csv[bogota_cuenca_df_csv.IndiAcce=='4. Baja'][['NSE_5', 'Poblacion']].groupby('NSE_5').sum()['Poblacion'].values
+    
+    
+
+    animals=['giraffes', 'orangutans', 'monkeys']
+
+    fig = go.Figure(data=[
+        go.Bar(name='Baja', x=nse, y=y4, width=0.4, marker_color='#491874'),
+        go.Bar(name='Media-Baja', x=nse, y=y3, width=0.4, marker_color='#a84276' ),
+        go.Bar(name='Media-Alta', x=nse, y=y2, width=0.4, marker_color='#eb8f6d'),
+        go.Bar(name='Alta', x=nse, y=y1, width=0.4, marker_color='#fcfdc6'),
+        # go.Bar(name='SF Zoo', x=animals, y=[20, 14, 23]),
+        # go.Bar(name='LA Zoo', x=animals, y=[12, 18, 29])
+    ])
+    # Change the bar mode
+    
+
+    # fig.update_yaxes(visible=False, showticklabels=False)
+    # fig.update_xaxes(visible=False, showticklabels=False)
+    
+    fig.update_layout(
+        xaxis_title="Nivel socioeconómico",
+        yaxis_title="Habitantes",
+        legend_title="Accesibilidad",
+        margin=dict(l=0, r=0, t=20, b=20),
+        barmode='stack',
+        # plot_bgcolor='white',
+        paper_bgcolor='#323232',
+        plot_bgcolor="#323232",
+        font=dict(
+            color="white"
+        ),
+
+    )
+    
+    # fig.update_layout(showlegend=False)
+
+    # fig.update_traces(marker_sizemin=3, selector=dict(type='scatter'))
+
+    # fig.update_coloraxes(colorbar_orientation='h')
+    # fig.update_coloraxes(colorbar_thickness=10)
+    # fig.update_coloraxes(colorbar_title=dict(text=''))
+    # fig.update_coloraxes(colorbar_y=0.0)
+    # fig.update_coloraxes(colorbar_x=0.5)
+    # fig.update_coloraxes(colorbar_len=0.3)
+    # fig.update_coloraxes(colorbar_tickfont=dict(color="#f4f4f4"))
+
+    return fig
 
 
 # Create a Dash app
@@ -166,7 +229,9 @@ app.layout = html.Div(
                     html.Div(
                         children=[
                             html.H5("Accesibilidad en Bogotá y Cuenca",
-                                    className="card-title"),
+                                    className="card-title",
+                                    style={'margin-bottom': '16px', }
+                                    ),
                             html.P(
                         "       Esta plataforma busca responder"
                                 " ¿cómo es la accesibilidad en Bogotá según el tipo de usuario?"
@@ -175,18 +240,36 @@ app.layout = html.Div(
                                 "según su modo de transporte",
                                 className="card-text",
                             ),
-                            html.H6('Ciudad'),
+                            html.H6('Seleccione un ciudad:'),
                             dcc.Dropdown(
                                 options=list(constants.CENTER_CITY_COORDINATES.keys()),
                                 value=list(constants.CENTER_CITY_COORDINATES.keys())[0],
                                 id=constants.CITY_SELECTOR,
-                                clearable=False
+                                clearable=False,
+                                style={'margin-bottom': '24px', }
                             ),
-                            html.H6('Variable'),
+                            
+                            html.H6('¿Qué desea ver?'),
+                            dcc.RadioItems(
+                                options=[
+                                    {'label': ' Accesibilidad', 'value': 'ACC'},
+                                    {'label': ' Población', 'value': 'POB'},
+                                ],
+                                value='ACC',
+                                labelStyle={'margin-right': '12px', },
+                                style={'margin-bottom': '12px', },
+                                id=constants.CATEGORY_SELECTOR,
+                            ),
+                            
+                            html.H6('  Seleccione una variable:'),
                             dcc.Dropdown(
-                                options=constants.CATEGORICAL_VARIABLES+constants.NON_CATEGORICAL_VARIABLES,
-                                value=constants.CATEGORICAL_VARIABLES[1],
-                                id='my-variable-selector',
+                                #options=constants.CATEGORICAL_VARIABLES+constants.NON_CATEGORICAL_VARIABLES,
+                                options=[
+                                    # {'label': 'Nivel socioeconómico', 'value': 'NSE_5'},
+                                    {'label': 'Índice de accesibilidad', 'value': 'IndiAcce'},
+                                ],
+                                value=constants.CATEGORICAL_VARIABLES[0],
+                                id=constants.VARIABLE_SELECTOR,
                                 clearable=False
                             ),
                             # html.H6('Medio de acceso'),
@@ -222,21 +305,29 @@ app.layout = html.Div(
                             ),
                         ],
                     ),
-                    # dbc.Row(
-                    #     [html.P("Seleccione un punto de la gráfica para ver los datos de una localidad.", className='graph-selector-text'),
-                    #      dcc.Graph(
-                    #         id=constants.SCATTER_ID,
-                    #         figure=get_selector_graph(
-                    #             list(constants.CENTER_CITY_COORDINATES.keys())[0],
-                    #             list(constants.ACCESIBILITY_MEANS.keys())[0]
-                    #         ),
-                    #         config={
-                    #             'displayModeBar': False
-                    #         },
-                    #         style={"height": "100%",
-                    #                "width": "100%"},
-                    #     )],
-                    # ),
+                    dbc.Row(
+                        children=[
+                            html.Div(
+                        # html.P("Seleccione un punto de la gráfica para ver los datos de una localidad.", className='graph-selector-text'),
+                                children=
+                                    [dcc.Graph(
+                                        id=constants.SCATTER_ID,
+                                        figure=get_bar_figure(),
+                                        # figure=get_selector_graph(
+                                        #     list(constants.CENTER_CITY_COORDINATES.keys())[0],
+                                        #     list(constants.ACCESIBILITY_MEANS.keys())[0]
+                                        # ),
+                                        config={
+                                            'displayModeBar': False
+                                        },
+                                        style={"height": "100%",
+                                               "width": "95%"},
+                                    )
+                                ],
+                                className='bar-content'
+                            )
+                        ],
+                    ),
                 ],
                 className='map-container'
             ),
@@ -248,22 +339,35 @@ app.layout = html.Div(
 
 
 @app.callback(
-        Output(component_id=constants.MAP_ID, component_property='figure'),
+        [
+            Output(component_id=constants.VARIABLE_SELECTOR, component_property='options'),
+            Output(component_id=constants.VARIABLE_SELECTOR, component_property='value'),
+            Output(component_id=constants.MAP_ID, component_property='figure'),
+        ],
     
         [
-        Input(component_id=constants.CITY_SELECTOR, component_property='value'),
-        Input(component_id='my-variable-selector', component_property='value')
-        
-    ],
+            Input(component_id=constants.CITY_SELECTOR, component_property='value'),
+            Input(component_id=constants.CATEGORY_SELECTOR, component_property='value'),
+            Input(component_id=constants.VARIABLE_SELECTOR, component_property='value')
+            
+        ],
         # Input(component_id=constants.ACCESIBILITY_SELECTOR,
         #       component_property='value'),
         # Input(component_id=constants.SCATTER_ID,
         #       component_property='hoverData'),
     
 )
-def update_output_div(city, variable):
+def update_output_div(city, category, variable):
     triggered_input = ctx.triggered_id
-    return update_hex_map(city=city, fig_hex_map=fig_hex_map, variable=variable)
+    if triggered_input == constants.CATEGORY_SELECTOR:
+        if category == 'ACC':
+            variable_options = [{'label': 'Índice de accesibilidad', 'value': 'IndiAcce'},]
+            variable = constants.CATEGORICAL_VARIABLES[0]
+        if category == 'POB':
+            variable_options = [{'label': 'Nivel socioeconómico', 'value': 'NSE_5'},]
+            variable = constants.CATEGORICAL_VARIABLES[1]
+        return variable_options, variable, update_hex_map(city=city, fig_hex_map=fig_hex_map, variable=variable)
+    return dash.no_update, dash.no_update, update_hex_map(city=city, fig_hex_map=fig_hex_map, variable=variable)
     
 
 
